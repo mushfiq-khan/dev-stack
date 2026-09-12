5ebec7643d8fe0047c7c3e6d034c10f6e0552f2f
mushfiq-khan <mushfiqkhan.developer@gmail.com>
2026-09-13 01:51:28 +0600

    first commit

---
+++ B14-A05-DevStack-main/a05-devstack/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './components/nav';
+import Hero from './Hero';
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
